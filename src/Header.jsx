b0c457f6e097baf9f7d7071b7909bc257f875142
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-06-15 13:35:30 -0300

    corrigindo alteração

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,48 +1,43 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-mixed-spaces-and-tabs */
+import React, { useState } from 'react';
+import './Header.css';
+import './App.css'
+import { Link } from 'react-router-dom';
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      toggleMenu();
-    }
-  };
-
-  return (
-    <header className='container'>
-      <Link to="/">
-        <img className='logo' src="icon/azaleia.png" alt="Ícone da logo" />
-      </Link>
-      <div className='wrapper'>
-        <Link to="/" className="logo-link">
-          <h1>Azalleia</h1>
-        </Link>
-      </div>
-      <nav className={`Nav ${menuOpen ? 'show' : ''}`}>
-        <Link to="/">Pagina Inicial</Link>
-        <Link to="/src/PAGINAS/Catalago.jsx">Catalago</Link>
-        <Link to="/src/PAGINAS/Lancament.jsx">Lançamentos</Link>
-        <Link to="/src/PAGINAS/Contatos.jsx">Contato</Link>
-      </nav>
-      <button
-        className="hamburger"
-        onClick={toggleMenu}
-        onKeyDown={handleKeyDown}
-        tabIndex={0}
-      >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </button>
-    </header>
-  );
+    return (
+        <header className='container'>
+            <Link to="/">
+                <img className='logo' src="/src/icon/azaleia.png"alt="Ícone da logo"/>
+            </Link>
+            <div className='wrapper'>
+            <Link to="/" className="logo-link">
+                    <h1>Azalleia</h1>
+                </Link>
+            </div>
+            <nav className={`Nav ${menuOpen ? 'show' : ''}`}>
+                <Link to="/">Pagina Inicial</Link>
+                <Link to="/src/PAGINAS/Catalago.jsx">Catalago</Link>
+                <Link to="/src/PAGINAS/Lancament.jsx">Lançamentos</Link>
+                <Link to="/src/PAGINAS/Contatos.jsx">Contato</Link>
+                
+            </nav>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
