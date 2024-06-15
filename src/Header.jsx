25c77c327e0e2d20e36ee6fa3522be29f771808f
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-06-15 14:59:30 -0300

    comit

--- src/Header.jsx
+++ src/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
     return (
         <header className='container'>
             <Link to="/">
-                <img className='logo' src="/src/Imagens/icon/azaleia.png"alt="Ícone da logo"/>
+                <img className='logo' src="imagen/icones/azaleia.png"alt="Ícone da logoo"/>
             </Link>
             <div className='wrapper'>
             <Link to="/" className="logo-link">
