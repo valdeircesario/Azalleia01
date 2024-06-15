9128b880fdb3380dd92eaebdb479b960063e9336
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-06-15 17:18:08 -0300

    caminho de imagem

--- src/Header.jsx
+++ src/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
     return (
         <header className='container'>
             <Link to="/">
-                <img className='logo' src="src/imagen/icones/azaleia.png"alt="Ícone logo"/>
+                <img className='logo' src="/public/icom/azaleia.png"alt="logo"/>
             </Link>
             <div className='wrapper'>
             <Link to="/" className="logo-link">
